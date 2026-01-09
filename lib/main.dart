0c14f3238e1d92f641c02e8c80def646ad1ba1da
ALEKSANDR <hhhs2332@gmail.com>
2026-01-10 01:15:15 +0900

    check commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AI Translator',
+      title: 'AI Translators',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.deepPurple,
